d31733f279c80bbdc5a0d6e3693bd1eeca921fee
ViniciusZevarex <vinicius.zevarex2002@gmail.com>
2018-10-09 12:49:54 -0300

    implementacao do carrinho!

--- servicos/carrinhoServico.php
+++ servicos/carrinhoServico.php
@@ -1,14 +1,29 @@
 <?php
 
-function addCarrinho($id){
-	if (!isset($_SESSION["carrinho"])) {
-		$_SESSION["carrinho"] = array();
-	}
-	if (!in_array($id, $_SESSION["carrinho"])) {
-			$_SESSION["carrinho"][] = $id;
-		redirecionar("produto/index");
-	}else{
-		redirecionar("produto/index");
-	}
+function addCarrinho($id) {
+    if (!isset($_SESSION["carrinho"])) {
+        $_SESSION["carrinho"] = array();
+    }
+
+    $existeProduto = false;
+
+    //procura se existe o produto no carrinho, e aumenta a quantidade!
+    foreach ($_SESSION["carrinho"] as $idCarrinho => $produto) {
+        if ($produto["id"] == $id) {
+            $produto["quantidade"]++;
+            
+            $_SESSION["carrinho"][$idCarrinho] = $produto; //salvando a alteracao no carrinho!
+            $existeProduto = true;
+            break;
+        }
+    }
+    //se nao existe produto no carrinho, cria um
+    if (!$existeProduto) {
+        $produto["id"] = $id;
+        $produto["quantidade"] = 1;
+        $_SESSION["carrinho"][] = $produto;
+    }
+
 }
+
 ?>
