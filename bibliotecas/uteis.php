6c2b1bfd7218eb46657fa6f8e7d0f8252e0239e4
LucasCavalherie <lucascavalherie01@gmail.com>
2018-11-16 15:12:57 -0200

    implemento do SQL_injection na função ehpost

--- bibliotecas/uteis.php
+++ bibliotecas/uteis.php
@@ -1,5 +1,12 @@
 <?php
 
 function ehPost() {
-    return $_SERVER["REQUEST_METHOD"] == "POST";
+	$ehPost = $_SERVER["REQUEST_METHOD"] == "POST";
+	if($ehPost) {
+		foreach ($_POST as $key => $value) {
+			$_POST[$key] = mysqli_real_escape_string(conexao(),$value);
+		}
+	}
+
+    return $ehPost;
 }
--- servicos/CnxReadConf.php
+++ servicos/CnxReadConf.php
@@ -1,7 +1,7 @@
 <?php
 	function rootAcess(){
 		$linha = array();
-		$arq = fopen("C:/wamp64/www/amazing_eletros_with_MVC/servicos/confCnx.txt", "r");
+		$arq = fopen("./amazing_eletros_with_MVC/servicos/confCnx.txt", "r");
 		while (!feof($arq)) {
 			$linha[] = trim(fgets($arq));
 		}
